f3630918a1d48b0fac5fbd096cd30c646fc1b4e7
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-08-27 09:52:43 +0200

    Access

--- AccessibilityText/AccessibilityTextApp.swift
+++ AccessibilityText/AccessibilityTextApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AccessibilityTextApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AccessibilityTextView()
         }
     }
 }
