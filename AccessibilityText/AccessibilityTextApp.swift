eafe51aefab2ec7498b943294fc79cb6ccddf529
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-08-27 08:41:23 +0200

    Initial Commit

---
+++ AccessibilityText/AccessibilityTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityTextApp.swift
+//  AccessibilityText
+//
+//  Created by Alexander Sobolev on 27.8.23..
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilityText/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AccessibilityText
+//
+//  Created by Alexander Sobolev on 27.8.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
